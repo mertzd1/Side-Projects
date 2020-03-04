8fe8fc0f332b972102814cf06a0dd866d4c9af5a
DonMertz1 <donald.mertz1@gmail.com>
2020-03-03 19:21:25 -0500

    WindowsPresentationFoundation

---
+++ Windows Presentation Foundation/WPF 09C ListBox/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace ListBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<Match> matches = new List<Match>();
+            matches.Add(new Match() { Team1 = "Detroit Lions", Team2 = "Tampa Bay Bucaneers", Score1 = 35, Score2 = 21, Completion = 49 });
+            matches.Add(new Match() { Team1 = "Denver Brocos", Team2 = "New England Patriots", Score1 = 18, Score2 = 14, Completion = 26 });
+            matches.Add(new Match() { Team1 = "Washington Redskins", Team2 = "Green Bay Packers", Score1 = 18, Score2 = 0, Completion = 55 });
+
+            lbMatches.ItemsSource = matches;
+        }
+        public class Match
+        {
+            public int Score1 { get; set; }
+            public int Score2 { get; set; }
+            public string Team1 { get; set; }
+            public string Team2 { get; set; }
+            public int Completion { get; set; }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(lbMatches.SelectedItem!=null)
+            {
+                MessageBox.Show("Selected Match: "
+                    + (lbMatches.SelectedItem as Match).Team1 + " " +
+                    (lbMatches.SelectedItem as Match).Score1 + " " +
+                    (lbMatches.SelectedItem as Match).Team2 + " " +
+                    (lbMatches.SelectedItem as Match).Score2);                    
+            }
+        }
+    }
+}
---
+++ Windows Presentation Foundation/WPF 11C Check Box/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_11C_Check_Box
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cbAllCheckedChanged(object sender, RoutedEventArgs e)
+        {
+            bool newval = (cbAllToppings.IsChecked == true);
+            cbHam.IsChecked = newval;
+            cbBacon.IsChecked = newval;
+            cbPineapple.IsChecked = newval;
+            cbMotzerella.IsChecked = newval;
+        }
+        private void cbSingleCheckChanged(object sender, RoutedEventArgs e)
+        {
+            cbAllToppings.IsChecked = null;
+            if((cbHam.IsChecked==true) && (cbBacon.IsChecked==true) && (cbPineapple.IsChecked==true)&& (cbMotzerella.IsChecked==true)) 
+            {
+                cbAllToppings.IsChecked =true;
+            }
+            if ((cbHam.IsChecked) == false && (cbBacon.IsChecked == false) && (cbPineapple.IsChecked == false) && (cbMotzerella.IsChecked == false))
+            {
+                cbAllToppings.IsChecked = false;
+            }
+
+
+        }
+    }
+}
